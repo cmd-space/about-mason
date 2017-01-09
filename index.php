42db6fe5c413177008b37a28a7da8cc18ee4cc22
Mason Crane <masonecrane@gmail.com>
2017-01-09 16:32:30 -0700

    Included jQuery via Google Hosted Libraries, added class of camo to hide elements, and beginning to create onclick function to make elements disappear

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 				content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
 		<link rel="stylesheet" href="style.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 		<title>Document</title>
 	</head>
 	<body>
@@ -17,9 +18,15 @@
 			I'll be brainstorming the many different apps I would love to create in order to contribute to our capstone project process. So many problems or 'problems' that need solving, and I can't wait to dig in.
 		</p>
 		<p>
-			I'm thinking about checking out Raja Yoga, since I have trouble focusing on occas... SQUIRREL! <img src="images/giphy%20(1).gif"
-																																				 alt="SQUIRREL!">
+			I'm thinking about checking out Raja Yoga, since I have trouble focusing on occas... SQUIRREL! <img id="gif" class="na" src="images/giphy%20(1).gif" alt="SQUIRREL!">
 		</p>
 		<img src="images/rajaYoga.jpg" alt="Raja Yoga Book Cover, written by Swami Vivekananda">
+		<script>
+			$(document).ready(function() {
+				$("#gif").click(function() {
+					this.("camo");
+				});
+			});
+		</script>
 	</body>
 </html>
