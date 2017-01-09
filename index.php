c21044775d8b8aa39f48d63406eba5cef45f0873
Mason Crane <masonecrane@gmail.com>
2017-01-09 15:53:50 -0700

    Added CSS file with reset and made some tweaks to p, h1, body and img. Also added .gif for fun.

--- index.php
+++ index.php
@@ -5,9 +5,11 @@
 		<meta name="viewport"
 				content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<link rel="stylesheet" href="style.css">
 		<title>Document</title>
 	</head>
 	<body>
+		<h1>about-mason</h1>
 		<p>
 			Lorem ipsum... Just kidding! It's a beautiful day to be stuck inside all day learning about programming and actually 'coding' on the first day of class. Maybe I'll be able to make this page look nice...
 		</p>
@@ -15,7 +17,8 @@
 			I'll be brainstorming the many different apps I would love to create in order to contribute to our capstone project process. So many problems or 'problems' that need solving, and I can't wait to dig in.
 		</p>
 		<p>
-			I'm thinking about checking out Raja Yoga, since I have trouble focusing on occas... SQUIRREL!
+			I'm thinking about checking out Raja Yoga, since I have trouble focusing on occas... SQUIRREL! <img src="images/giphy%20(1).gif"
+																																				 alt="SQUIRREL!">
 		</p>
 		<img src="images/rajaYoga.jpg" alt="Raja Yoga Book Cover, written by Swami Vivekananda">
 	</body>
